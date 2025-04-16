e5e22ffed49b156ceca9879801e280f056fc01b6
iamsondev <sondipkumarsk@gmail.com>
2025-04-17 00:44:59 +0600

    added DaisyCard

--- src/Components/PriceOption/PriceOption.jsx
+++ src/Components/PriceOption/PriceOption.jsx
@@ -1,5 +1,6 @@
 import React, { use } from 'react';
 import PricingCard from '../PricingCard/PricingCard';
+import DaisyCard from '../DaisyCard/DaisyCard';
 
 const PriceOption = ({ pricePromise }) => {
   const pricingData = use(pricePromise);
@@ -7,13 +8,16 @@ const PriceOption = ({ pricePromise }) => {
   return (
     <div>
       <h1 className='text-5xl mt-10 m-5 '>Get your Plan</h1>
-      <div className='grid grid-cols-3 gap-2'>
-        {
+      <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-2'>
+        {/* {
           pricingData.map((pricing, index) => <PricingCard 
           key={index} 
           pricing={pricing}>
 
           </PricingCard>)
+        } */}
+        {
+          pricingData.map(pricing => <DaisyCard pricing={pricing}></DaisyCard>)
         }
       </div>
 
--- src/Components/PricingCard/PricingCard.jsx
+++ src/Components/PricingCard/PricingCard.jsx
@@ -1,18 +1,21 @@
 import React from 'react';
 import Feature from '../Feature/Feature';
 
-const PricingCard = ({pricing}) => {
-  const {name, duration, price,description, features}= pricing;
+const PricingCard = ({ pricing }) => {
+  const { name, price, description, features } = pricing;
   return (
-    <div className='border bg-green-600 p-5 rounded-2xl mt-2'>
+    <div className='border bg-violet-900 p-5 rounded-2xl mt-2 mb-10'>
+      <div>
         <h1 className='text-3xl mb-2'>{name}</h1>
-        <h3 className='text-2xl mb-3'>{price}</h3>
-        <h4 className='text-xl mb-3'>{duration}</h4>
+        <h3 className='text-2xl mb-3'>{price}</h3> 
+      </div>
+      <div className='bg-violet-700 p-4 rounded-2xl mt-10 flex-1'>
         <p className='mb-2'>{description}</p>
         {
-           features.map((feat) => <Feature feat={feat}></Feature> )
+          features.map((feat) => <Feature feat={feat}></Feature>)
         }
-
+      </div>
+      <button className="btn w-full bg-amber-600 mt-5">SubsCribe</button>
     </div>
   );
 };
